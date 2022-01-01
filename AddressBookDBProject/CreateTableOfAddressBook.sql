6a8410debf7101b03efc0431c202f19c3ce095ad
Wpaswan <dablumcapaswan@gmail.com>
2021-12-31 22:09:16 -0800

    [I modified the code to create table of address book]

---
+++ AddressBookDBProject/CreateTableOfAddressBook.sql
@@ -0,0 +1,14 @@
+﻿create table AddressBook
+( id int identity(1,1) not null,
+  firstName varchar(100) ,
+  lastName varchar(100),
+  address varchar(200),
+  city varchar(100),
+  state varchar(100),
+  zip int,
+  phoneNumber varchar(300),
+  email varchar(300)
+
+
+)
+select *from AddressBook
